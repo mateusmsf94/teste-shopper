0bc6ddacf60cf5a90acdbac9ba198c8bc8b755ad
Mateus M. S. A. Figueiredo <mateusmelosf@gmail.com>
2023-05-15 23:44:22 -0300

    fix database config

--- backend/src/database/config/config.ts
+++ backend/src/database/config/config.ts
@@ -2,5 +2,5 @@ export default {
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || '123456',
-  
+  database: process.env.DB_NAME || 'SHOPPER',
 };
