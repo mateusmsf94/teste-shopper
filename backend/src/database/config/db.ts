5e0c7792a0dca8adf04bd7feac17ca14b4afd3f5
Mateus M. S. A. Figueiredo <mateusmelosf@gmail.com>
2023-05-15 22:35:07 -0300

    script inicializacao do db

--- backend/src/database/config/db.ts
+++ backend/src/database/config/db.ts
@@ -5,7 +5,6 @@ const pool: Pool = createPool({
   host: config.host,
   user: config.user,
   password: config.password,
-  database: config.database,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
